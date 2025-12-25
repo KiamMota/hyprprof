e9b09d8f96382f1d7f57bba441568a354bcce28f
KiamMota <somentekiam@gmail.com>
2025-12-25 14:50:26 -0300

    refac : project

---
+++ build/CMakeFiles/hyprprof.dir/cmake_clean.cmake
@@ -0,0 +1,20 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hyprprof.dir/link.d"
+  "CMakeFiles/hyprprof.dir/src/environment.cpp.o"
+  "CMakeFiles/hyprprof.dir/src/environment.cpp.o.d"
+  "CMakeFiles/hyprprof.dir/src/fs.cpp.o"
+  "CMakeFiles/hyprprof.dir/src/fs.cpp.o.d"
+  "CMakeFiles/hyprprof.dir/src/json/json_file_parser.cpp.o"
+  "CMakeFiles/hyprprof.dir/src/json/json_file_parser.cpp.o.d"
+  "CMakeFiles/hyprprof.dir/src/json/log.cpp.o"
+  "CMakeFiles/hyprprof.dir/src/json/log.cpp.o.d"
+  "CMakeFiles/hyprprof.dir/src/main.cpp.o"
+  "CMakeFiles/hyprprof.dir/src/main.cpp.o.d"
+  "hyprprof"
+  "hyprprof.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hyprprof.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
