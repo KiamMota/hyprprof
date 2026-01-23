677d75b9fe298aed3388dc433a8577789d3b82ef
Kiam Mota <133913407+KiamMota@users.noreply.github.com>
2026-01-23 16:21:31 -0300

    Update json_schemas.hpp

--- include/core/json_schemas.hpp
+++ include/core/json_schemas.hpp
@@ -1,31 +1,30 @@
 #ifndef JSON_SCHEMAS_HPP
 #define JSON_SCHEMAS_HPP
 
-#define HYPRPROF_JSON_SCHEMA                                                                       \
-    R"(
+#define HYPRPROF_JSON_SCHEMA \
+R"(
 {
   "$schema": "http://json-schema.org/draft-07/schema#",
   "title": "Hyprland Profile Schema",
   "type": "object",
-  "required": ["hyprprof", "version_constraints", "build", "components", "providers"],
+  "required": ["hyprprof", "components", "providers"],
   "properties": {
     "hyprprof": {
-  "type": "object",
-  "required": ["authors", "name", "version", "description"],
-  "properties": {
-    "authors": {
-      "type": "array",
-      "minItems": 1,
-      "items": { "type": "string" }
+      "type": "object",
+      "required": ["authors", "name", "version"],
+      "properties": {
+        "authors": {
+          "type": "array",
+          "minItems": 1,
+          "items": { "type": "string" }
+        },
+        "name": { "type": "string" },
+        "version": { "type": "string", "pattern": "^\\d+\\.\\d+\\.\\d+$" },
+        "description": { "type": "string" }
+      }
     },
-    "name": { "type": "string" },
-    "version": { "type": "string", "pattern": "^\\d+\\.\\d+\\.\\d+$" },
-    "description": { "type": "string" }
-    }
-  },
     "version_constraints": {
       "type": "object",
-      "required": ["hyprland", "wayland"],
       "properties": {
         "hyprland": { "type": "string", "pattern": "^\\^?\\d+\\.\\d+\\.\\d+$" },
         "wayland": { "type": "string", "pattern": "^\\^?\\d+\\.\\d+\\.\\d+$" }
@@ -33,11 +32,10 @@
     },
     "build": {
       "type": "object",
-      "required": ["install_script", "uninstall_script", "other_scripts"],
       "properties": {
         "install_script": { "type": "string" },
         "uninstall_script": { "type": "string" },
-        "other_scripts": {
+        "other_scripts": { 
           "type": "array",
           "items": { "type": "string" }
         }
@@ -51,7 +49,8 @@
         "terminal": { "type": "string" },
         "launcher": { "type": "string" },
         "notification": { "type": "string" },
-        "bar": { "type": "string" }
+        "bar": { "type": "string" },
+        "browser": { "type": "string" }
       }
     },
     "providers": {
