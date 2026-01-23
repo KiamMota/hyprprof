4de4a1ac14d73845eedec2e922f0e72655686272
KiamMota <somentekiam@gmail.com>
2026-01-23 00:19:24 -0300

    add : basic cli

---
+++ include/infra/version.hpp
@@ -0,0 +1,16 @@
+#ifndef VERSION_HPP
+#define VERSION_HPP
+
+namespace app {
+namespace version {
+
+    inline constexpr const char* VERSION = "0.1.0";
+    inline constexpr int VERSION_MAJOR = 1;
+    inline constexpr int VERSION_MINOR = 0;
+    inline constexpr int VERSION_PATCH = 0;
+
+}
+}
+
+
+#endif // VERSION_HPP
--- src/cli/main.cpp
+++ src/cli/main.cpp
@@ -1,13 +1,49 @@
+#include <cstring>
 #include <unistd.h>
-#include "CLI11/CLI/CLI.hpp"
+#include "infra/version.hpp"
+#include <iostream>
+
+void help() {
+    std::cout << "Hyprprof - CLI Profile Manager for Hyprland\n\n";
+    std::cout << "Usage:\n";
+    std::cout << "  hyprprof -h, --help           Show this help message\n";
+    std::cout << "  hyprprof -v, --version        Show the application version\n\n";
+
+    std::cout << "Commands:\n";
+    std::cout << "  make <name>                   Create a new profile with the specified name\n";
+    std::cout << "  install <path/url>            Install a profile from a path or URL\n";
+    std::cout << "  info                          Show information about the current profile\n";
+    std::cout << "  uninstall | delete            Delete the current profile\n\n";
+
+    std::cout << "Examples:\n";
+    std::cout << "  hyprprof make default\n";
+    std::cout << "  hyprprof install https://myprofile.com\n";
+    std::cout << "  hyprprof info\n";
+    std::cout << "  hyprprof uninstall\n";
+}
+
 
 int main(int argc, char** argv) {
-  CLI::App app{"profile manager to hyprland"};
-  argv = app.ensure_utf8(argv);
+  
+    if(argc == 1)
+    {
+      help();
+      return 0;
+    }
 
-    std::string filename = "default";
-    app.add_option("-f,--file", filename, "A help string");
-  CLI11_PARSE(app, argc, argv);
+    if(argc > 1)
+    {
+      if(strcmp(argv[1], "-v") == 0 || strcmp(argv[1], "--version") == 0)
+      {
+        std::cout << app::version::VERSION << "\n";
+        return 0;  
+      }
+      if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0)
+      {
+        help();
+        return 0;
+      } 
+    }
     return 0;
 }
 
