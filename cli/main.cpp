cdf1b86e87bd4a0359d9d04527c6e3045f9906cc
KiamMota <somentekiam@gmail.com>
2026-01-24 14:31:41 -0300

    cli cmake

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,8 +1,30 @@
+#include "infra/version.hpp"
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
+    if (argc < 2) {
+        std::cout << "No arguments provided. Use -h or --help for usage.\n";
+        return 0;
+    }
 
+    std::string arg = argv[1];
 
+    if (arg == "-h" || arg == "--help") {
+        std::cout << "Usage: mycli [options]\n";
+        std::cout << "  -h, --help      Show this help message\n";
+        std::cout << "  -v, --version   Show version info\n";
+    } 
+    else if (arg == "-v" || arg == "--version") {
+      std::cout << app::version::VERSION << std::endl;
+    } 
+    else {
+        std::cout << "Unknown option: " << arg << "\n";
+        std::cout << "Use -h or --help for usage.\n";
+        return 1;
+    }
 
+    return 0;
 }
+
