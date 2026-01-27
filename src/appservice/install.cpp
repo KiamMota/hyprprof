dcf476d3df3bc7edf72b8c627314d6c8f5013a69
KiamMota <somentekiam@gmail.com>
2026-01-26 21:06:43 -0300

    refac: install service

--- src/appservice/install.cpp
+++ src/appservice/install.cpp
@@ -4,7 +4,6 @@
 #include "core/json_current_profile.hpp"
 #include "core/json/json_manifest_reader.hpp"
 #include "core/profile/exceptions.hpp"
-#include "core/profile/profile.hpp"
 #include "core/profile/profile_assembler.hpp"
 #include "infra/fs/dir.hpp"
 #include "infra/fs/file.hpp"
@@ -17,15 +16,12 @@
 app_service::Install::Install(const std::string& curr_path) {
 
     std::string hyprprof_json_path = infra::fs::dir::get_absolute(curr_path) + "/hyprprof.json";
-    if(!infra::fs::dir::is_dir(hyprprof_json_path))
-    {
-      infra::hypr_log::err(hyprprof_json_path + " doesn't exist.");
-    return;
-    }
+
     if (!infra::fs::file::exists(hyprprof_json_path)) {
-        infra::hypr_log::err("hyprprof.json not found");
+        infra::hypr_log::err(hyprprof_json_path + " doesn't exist.");
         return;
     }
+
     std::string json_str = infra::fs::file::get_content(hyprprof_json_path);
 
     core::json::JSONManifestReader json_val{json_str};
@@ -61,19 +57,18 @@ app_service::Install::Install(const std::string& curr_path) {
     infra::fs::dir::create(profile_path);
     infra::fs::file::move(hyprprof_json_path, profile_path);
 
- 
     core::CurrentProfile current_profile{};
     current_profile.set_current_path(hyprprof_json_path);
     current_profile.set_profile_name(profile_path);
 
     core::JsonCurrentProfile curr_prof_json{};
     std::string curr_proj = curr_prof_json.make_json(hyprprof_json_path, profile_path);
-  
+
     infra::fs::dotconfig::create(".hyprprof_current.json");
-    std::string json_hyprprof_current_path = infra::fs::dotconfig::app_path(".hyprprof_current.json");
+    std::string json_hyprprof_current_path =
+        infra::fs::dotconfig::app_path(".hyprprof_current.json");
 
     infra::fs::file::overwrite(json_hyprprof_current_path, curr_proj);
-
 
     infra::hypr_log::ok("profile installed!");
     infra::hypr_log::log("to use the profile: hyprprof use");
