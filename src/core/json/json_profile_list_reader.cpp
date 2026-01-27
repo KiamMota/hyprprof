eef09c04c6161360f230c3177de1b8a29cbf8430
KiamMota <somentekiam@gmail.com>
2026-01-26 22:27:59 -0300

    afipbmfbm

--- src/core/json/json_profile_list_reader.cpp
+++ src/core/json/json_profile_list_reader.cpp
@@ -1,9 +1,9 @@
-#include "core/json/json_current_profile_reader.hpp"
+#include "core/json/json_profile_list_reader.hpp"
 #include "infra/json.hpp"
 #include "rapidjson/document.h"
 #include <stdexcept>
 
-core::json::JSONCurrentProfileReader::JSONCurrentProfileReader(const std::string& json_src)
+core::json::JSONProfileListReader::JSONProfileListReader(const std::string& json_src)
 {
   if(json_src.empty())
     throw std::runtime_error("json source is empty");
@@ -12,12 +12,12 @@ core::json::JSONCurrentProfileReader::JSONCurrentProfileReader(const std::string
   doc.Parse(json_src.c_str());
 }
 
-const std::string core::json::JSONCurrentProfileReader::current_profile_name() const noexcept
+const std::string core::json::JSONProfileListReader::current_profile_name() const noexcept
 {
   return doc["current_profile"].GetString();
 }
 
-const std::string core::json::JSONCurrentProfileReader::current_profile_path() const noexcept
+const std::string core::json::JSONProfileListReader::current_profile_path() const noexcept
 {
   return doc["current_path"].GetString();
 }
