886702b9ceccfd1d20dc1bca53019e1dd07b6fbf
KiamMota <somentekiam@gmail.com>
2026-01-28 11:24:21 -0300

    update install script

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ BUILD_DIR="$PROJECT_ROOT/build"
 BIN_NAME="hyprprof"
 INSTALL_PATH="/usr/local/bin/$BIN_NAME"
 
+CONFIG_DIR="$HOME/.config/hyprprof"
+CONFIG_FILE="$CONFIG_DIR/profiles.json"
+
 echo "[*] Building $BIN_NAME"
 
 command -v cmake >/dev/null || { echo "cmake not found"; exit 1; }
@@ -17,12 +20,16 @@ cd "$BUILD_DIR"
 cmake ..
 make -j"$(nproc)"
 
-if [[ ! -f "$BIN_NAME" ]]; then
-  echo "Binary was not generated"
-  exit 1
+# Descobre o caminho real do binário
+BIN_PATH=$(find . -type f -executable -name "$BIN_NAME" | head -n 1)
+
+if [[ -z "$BIN_PATH" ]]; then
+    echo "Binary was not generated"
+    exit 1
 fi
 
-echo "[*] Installing to $INSTALL_PATH"
-sudo install -m 755 "$BIN_NAME" "$INSTALL_PATH"
 
-echo "[✓] $BIN_NAME installed successfully"
+# Instala binário
+echo "[*] Installing $BIN_PATH to $INSTALL_PATH"
+sudo install -m 755 "$BIN_PATH" "$INSTALL_PATH"
+
