34de01a90381f18cc431fd3a96bc3e1c010c7198
KiamMota <somentekiam@gmail.com>
2026-02-14 16:25:32 -0300

    update version

--- include/utils/log.hpp
+++ include/utils/log.hpp
@@ -7,45 +7,43 @@
 
 namespace hypr_log {
 
-template <typename... Args>
-void log(const std::string& s, Args&&... args) {
-    std::cout << "\033[47;30m" << "hyprprof:" << "\033[0m" << " "; 
+template <typename... Args> void log(const std::string& s, Args&&... args) {
+    std::cout << "\033[47;30m" << "hyprprof:" << "\033[0m" << " ";
     std::cout << s;
     (std::cout << ... << args);
     std::cout << "\n";
 }
 
-template <typename... Args>
-void err(const std::string& s, Args&&... args) {
-    std::cout << "\033[41;30m" << "hyprprof err:" << "\033[0m" << " "; // fundo vermelho, texto preto
+template <typename... Args> void err(const std::string& s, Args&&... args) {
+    std::cout << "\033[41;30m" << "hyprprof err:" << "\033[0m"
+              << " "; // fundo vermelho, texto preto
     std::cout << s;
     (std::cout << ... << args);
     std::cout << "\n";
 }
 
-template <typename... Args>
-void warn(const std::string& s, Args&&... args) {
-    std::cout << "\033[103;30m" << "hyprprof warn:" << "\033[0m" << " "; // fundo amarelo claro, texto preto
+template <typename... Args> void warn(const std::string& s, Args&&... args) {
+    std::cout << "\033[103;30m" << "hyprprof warn:" << "\033[0m"
+              << " "; // fundo amarelo claro, texto preto
     std::cout << s;
     (std::cout << ... << args);
     std::cout << "\n";
 }
-inline void file_not_exists(const std::string& file_path)
-{
+inline void file_not_exists(const std::string& file_path) {
     std::filesystem::path p(file_path);
     std::string file_name = p.filename().string(); // pega só o nome do arquivo
-    hypr_log::err(file_path + ": " "file named '" + file_name + "' doesn't exists");
+    hypr_log::err(file_path +
+                  ": "
+                  "file named '" +
+                  file_name + "' doesn't exists");
 }
-template <typename... Args>
-inline void ok(const std::string& s, Args&&... args) {
+template <typename... Args> inline void ok(const std::string& s, Args&&... args) {
     std::cout << "\033[42;30m" << "hyprprof:" << "\033[0m" << " "; // fundo verde, texto preto
     std::cout << s;
     (std::cout << ... << args);
     std::cout << "\n";
 }
 
-
 } // namespace hypr_log
-
 
 #endif
--- include/utils/version.hpp
+++ include/utils/version.hpp
@@ -4,13 +4,12 @@
 namespace app {
 namespace version {
 
-    inline constexpr const char* VERSION = "0.1.0";
-    inline constexpr int VERSION_MAJOR = 1;
-    inline constexpr int VERSION_MINOR = 0;
-    inline constexpr int VERSION_PATCH = 0;
+inline constexpr const char* VERSION = "0.1.1";
+inline constexpr int VERSION_MAJOR = 0;
+inline constexpr int VERSION_MINOR = 1;
+inline constexpr int VERSION_PATCH = 1;
 
-}
-}
-
+} // namespace version
+} // namespace app
 
 #endif // VERSION_HPP
